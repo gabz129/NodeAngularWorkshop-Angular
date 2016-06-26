24f81b63577ad9339e399ca6c1f0636eb88a804a
Gabz129 <gabriellopezcondori@gmail.com>
2016-06-26 15:05:54 -0300

    added filter, added get band details

--- src/components/app/routes.js
+++ src/components/app/routes.js
@@ -17,19 +17,29 @@ export function routes($routeProvider, $locationProvider) {
             templateUrl: '/components/app/views/index.html',
             resolve: {
                 // use resolve to to pass data to the template under $resolve
-                data: () => new Promise(resolve => resolve(bands)), // mock data
+                //data: () => new Promise(resolve => resolve(bands)), // mock data
+                data: (ApiService) => ApiService.getBands(), 
             },
         })
         .when('/band/:bandId/', {
             templateUrl: '/components/app/views/band-detail.html',
             resolve: {
                 // use resolve to to pass data to the template under $resolve
+                //band: () => new Promise(resolve => resolve(bands[0])),
+                //band: (ApiService) => ApiService.getBand($routeParams.bandId),
+                band: (ApiService) => ApiService.getBand(0),
+                artists: (ApiService) => ApiService.getArtists(),
+                albums: (ApiService) => ApiService.getAlbums(),
             },
         })
         .when('/band/:bandId/album/:albumId/', {
             templateUrl: '/components/app/views/band-detail.html',
             resolve: {
                 // use resolve to to pass data to the template under $resolve
+                band: (ApiService) => ApiService.getBand(0),
+                artists: (ApiService) => ApiService.getArtists(),
+                albums: (ApiService) => ApiService.getAlbums(),
+                tracks: (ApiService) => ApiService.getTracks(0, 0),
             },
         })
         .when('/band/:bandId/album/:albumId/track/:trackId/', {
--- src/components/app/services/api.js
+++ src/components/app/services/api.js
@@ -1,3 +1,9 @@
+import {bands} from './mock-data';
+import {artists} from './mock-data';
+import {albums} from './mock-data';
+import {tracks} from './mock-data';
+
+
 export class ApiService {
     url = 'http://localhost:1337/';
 
@@ -6,4 +12,43 @@ export class ApiService {
         this.http = $http;
     }
 
+    getBands(){
+        /*return this.http.get( url + '/band/all').then(
+    	function(response){
+    		return response.bands
+    	});*/
+    	return new Promise(resolve => resolve(bands));
+    };
+
+    getBand(bandId){
+        /*return this.http.get(url + '/band/' + bandId).then(
+    	function(response){
+    		return response.band
+    	});*/
+    	return new Promise(resolve => resolve(bands[bandId]));
+    }
+
+    getArtists(){
+        /*return this.http.get(url + '/band/' + bandId + '/artist/all').then(
+            function(response){
+                return response.artists
+        });*/
+        return new Promise(resolve => resolve(artists));
+    }
+
+    getAlbums(){
+        /*return this.http.get(url + '/band/' + bandId + '/album/all').then(
+            function(response){
+                return response.albums
+        });*/
+        return new Promise(resolve => resolve(albums));
+    }
+
+    getTracks(bandId, albumId){
+        /*return this.http.get(url + '/band/' + bandId + '/album/' + albumId + '/tracks/all').then(
+            function(response){
+                return response.tracks
+        });*/
+        return new Promise(resolve => resolve(tracks));
+    }
 }
